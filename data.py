d0d85a611b7c0871215e680dbbc4c6883aeeac03
itzhim <85541689+itzhim@users.noreply.github.com>
2023-01-13 11:50:24 +0530

    Update data.py

--- data.py
+++ data.py
@@ -69,8 +69,8 @@ class NYU_BasicAugmentRGBSequence(Sequence):
 
             sample = self.dataset[index]
 
-            x = np.clip(np.asarray(Image.open( "nyu_data/"+sample[0] )).reshape(480,640,3)/255,0,1)
-            y = np.clip(np.asarray(Image.open( "nyu_data/"+sample[1] )).reshape(480,640,1)/255*self.maxDepth,0,self.maxDepth)
+            x = np.clip(np.asarray(Image.open( "/content/drive/MyDrive/nyu_data/"+sample[0] )).reshape(480,640,3)/255,0,1)
+            y = np.clip(np.asarray(Image.open( "/content/drive/MyDrive/nyu_data/"+sample[1] )).reshape(480,640,1)/255*self.maxDepth,0,self.maxDepth)
             y = DepthNorm(y, maxDepth=self.maxDepth)
 
             batch_x[i] = nyu_resize(x, 480)
