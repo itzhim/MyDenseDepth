c0000af55f31864fa4fdb209847dc733f3ca8859
itzhim <85541689+itzhim@users.noreply.github.com>
2023-01-14 17:14:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from matplotlib import pyplot as plt
 
 # Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
-parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
+parser.add_argument('--model', default='/content/model_5000.h5', type=str, help='Trained Keras model file.')
 parser.add_argument('--input', default='examples/1_image.png', type=str, help='Input filename or folder.')
 args = parser.parse_args()
 
@@ -40,8 +40,8 @@ outputs = predict(model, inputs)
 #matplotlib.use('TkAgg')   
 
 # Display results
-"""viz = display_images(outputs.copy(), inputs.copy())
+viz = display_images(outputs.copy(), inputs.copy())
 plt.figure(figsize=(10,5))
 plt.imshow(viz)
 plt.savefig('test.png')
-plt.show()"""
+plt.show()
