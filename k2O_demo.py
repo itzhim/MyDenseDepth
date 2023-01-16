08c4f7e23d4dab952617f6a8626d561dc898f9a6
itzhim <85541689+itzhim@users.noreply.github.com>
2023-01-16 14:39:11 +0530

    Update k2O_demo.py

--- k2O_demo.py
+++ k2O_demo.py
@@ -1,17 +1,21 @@
 import os
 os.environ['TF_KERAS'] = '1'
-
+import tensorflow as tf
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import load_model
 import onnx
-import keras2onnx
+#import keras2onnx
 from layers import BilinearUpSampling2D
+from tensorflow.keras.layers import BatchNormalization
 
 onnx_model_name = 'nyu.onnx'
 
 # Custom object needed for inference and training
-custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
+custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None, 'BatchNormalization': BatchNormalization}
 
-model = load_model('nyu.h5', custom_objects=custom_objects, compile=False)
-onnx_model = keras2onnx.convert_keras(model, model.name)
-onnx.save_model(onnx_model, onnx_model_name)
+model = load_model('/content/MyDenseDepth/models/1673859614-n2-e1-bs8-lr0.0001-densedepth_nyu/model.h5')
+#model = load_model('C:\\Users\\singhal\\Downloads\\DenseDepth-master\\model_5000\\model_5000.h5', custom_objects=custom_objects, compile=False)
+#onnx_model = keras2onnx.convert_keras(model, model.name)
+#onnx.save_model(onnx_model, onnx_model_name)
+
+tf.saved_model.save(model, "tmp_model")
