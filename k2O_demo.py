01ce4c6fef36a48e92e7b7cdfb44f6beb0bcd478
itzhim <85541689+itzhim@users.noreply.github.com>
2023-01-16 14:40:50 +0530

    Update k2O_demo.py

--- k2O_demo.py
+++ k2O_demo.py
@@ -13,7 +13,7 @@ onnx_model_name = 'nyu.onnx'
 # Custom object needed for inference and training
 custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None, 'BatchNormalization': BatchNormalization}
 
-model = load_model('/content/MyDenseDepth/models/1673859614-n2-e1-bs8-lr0.0001-densedepth_nyu/model.h5')
+model = load_model('/content/drive/MyDrive/model.h5')
 #model = load_model('C:\\Users\\singhal\\Downloads\\DenseDepth-master\\model_5000\\model_5000.h5', custom_objects=custom_objects, compile=False)
 #onnx_model = keras2onnx.convert_keras(model, model.name)
 #onnx.save_model(onnx_model, onnx_model_name)
